1c49590f8a7d5e4ba2d39600fd6d3e81ed13d673
Andy Garcia <andy@xurpas.com>
2023-04-05 17:33:48 +0800

    Added Auto Populating Origin field

--- datamanagement/data_management/services/services.py
+++ datamanagement/data_management/services/services.py
@@ -1,12 +1,19 @@
 import frappe,boto3
 
 @frappe.whitelist()
-def test_aws():
+def test_aws(name):
+    maindoc = frappe.get_doc('AWS Test',name)
     # Let's use Amazon S3
     s3 = boto3.resource('s3')
     # Print out bucket names
     buckets = []
     for bucket in s3.buckets.all():
+        bucketEntry = maindoc.append('test',{})
+    
         print(bucket.name)
         buckets.append(bucket.name)
+        bucketEntry.bucket=bucket.name
+
+       
+    maindoc.save()
     return str(buckets)
