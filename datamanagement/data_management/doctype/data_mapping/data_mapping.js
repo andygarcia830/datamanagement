a832fa936075770b7f48f171a04e4fff65bdc5d5
Andy Garcia <andy@xurpas.com>
2023-04-05 15:59:29 +0800

    Data Mapping bugfix

--- datamanagement/data_management/doctype/data_mapping/data_mapping.js
+++ datamanagement/data_management/doctype/data_mapping/data_mapping.js
@@ -24,4 +24,26 @@ frappe.ui.form.on("Data Mapping", {
     });
 
 	},
+    onload(frm) {
+        console.log("REFRESH")
+        if (frm.doc.metadata.length() > 0){
+            frappe.call({method:'datamanagement.data_management.doctype.data_mapping.data_mapping.fetch_values', args:{
+                'metadata':frm.doc.metadata,
+                'name':frm.doc.name
+            },
+            callback:function(r){
+                console.log(r.message)
+                const obj = r.message
+                frm.set_value('description',obj.description);
+                frm.set_value('source',obj.source);
+                frm.set_value('fields',obj.fields);
+                frm.set_value('url',obj.url);
+                frm.set_value('storage_type',obj.storage_type);
+                frm.set_value('creation_date',obj.creation_date);
+                frm.set_value('retention_period_units',obj.retention_period_units);
+                frm.set_value('retention_period_value',obj.retention_period_value);
+            }
+        });
+     }
+	},
 });
