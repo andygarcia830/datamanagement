bfb85ef69d66ffab0d8c6ba539ae256dd4c418f3
Andy Garcia <andy@xurpas.com>
2023-04-16 21:20:05 +0800

    updates

--- datamanagement/data_management/page/airflow/airflow.js
+++ datamanagement/data_management/page/airflow/airflow.js
@@ -13,19 +13,26 @@ frappe.pages['airflow'].on_page_load = function(wrapper) {
 		},
 		callback:function(r){
 			airflow_url=r.message;
-			//console.log("AIRFLOW URL="+airflow_url);
-			thisPage='<div class="h-100 d-flex align-items-center justify-content-center">\
-			<!--[if IE]><object classid="clsid:25336920-03F9-11CF-8FD0-00AA00686F13" data="'+airflow_url+'">\
-			<p>backup content</p>\
-			</object>\
-			<![endif]-->\
-			<!--[if !IE]> <-->\
-			<object type="text/html" data="'+airflow_url+'" style="width:100%; height:768px">\
-			<p>backup content</p>\
-			</object>\
-			<!--> <![endif]-->\
-			</div>';
-			renderPage(thisPage);
+			if (airflow_url == null || airflow_url == '' ){
+				renderPage("Airflow URL not defined. Please define it in Development Portal Settings.");
+
+			}
+
+			else {
+				//console.log("AIRFLOW URL="+airflow_url);
+				thisPage='<div class="h-100 d-flex align-items-center justify-content-center">\
+				<!--[if IE]><object classid="clsid:25336920-03F9-11CF-8FD0-00AA00686F13" data="'+airflow_url+'">\
+				<p>backup content</p>\
+				</object>\
+				<![endif]-->\
+				<!--[if !IE]> <-->\
+				<object type="text/html" data="'+airflow_url+'" style="width:100%; height:768px">\
+				<p>backup content</p>\
+				</object>\
+				<!--> <![endif]-->\
+				</div>';
+				renderPage(thisPage);
+				}
 
 		}
 		});
