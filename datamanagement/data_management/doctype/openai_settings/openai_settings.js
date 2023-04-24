9d518d5ae1df44de293eadd03ac7126eeaf53704
Andy Garcia <andy@xurpas.com>
2023-04-24 16:58:19 +0800

    Bugfix

--- datamanagement/data_management/doctype/openai_settings/openai_settings.js
+++ datamanagement/data_management/doctype/openai_settings/openai_settings.js
@@ -1,8 +1,15 @@
 // Copyright (c) 2023, Andy Garcia and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("OpenAI Settings", {
-// 	refresh(frm) {
+frappe.ui.form.on("OpenAI Settings", {
+	after_save(frm) {
+        frappe.call({method:'datamanagement.data_management.doctype.openai_settings.openai_settings.set_key', args:{
+            'key':frm.doc.openai_api_key
+        },
+        callback:function(r){
+            
+        }
+        });
 
-// 	},
-// });
+	},
+});
--- datamanagement/data_management/doctype/openai_settings/openai_settings.py
+++ datamanagement/data_management/doctype/openai_settings/openai_settings.py
@@ -1,8 +1,13 @@
 # Copyright (c) 2023, Andy Garcia and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe,os
 from frappe.model.document import Document
 
 class OpenAISettings(Document):
 	pass
+
+
+@frappe.whitelist()
+def set_key(key):
+	os.environ["OPENAI_API_KEY"] = key
