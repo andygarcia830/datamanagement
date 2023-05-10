f9d1a882c05abd09e4622d6505469c3afe4c3c6e
Andy Garcia <andy@xurpas.com>
2023-05-10 22:25:49 +0800

    added DataSink

--- datamanagement/data_management/doctype/datasink/datasink.js
+++ datamanagement/data_management/doctype/datasink/datasink.js
@@ -1,41 +1,9 @@
 // Copyright (c) 2023, Andy Garcia and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on("DataSource", {
+
+frappe.ui.form.on("DataSink", {
 	refresh(frm) {
-
-        show_access_buttons();
-        
-
-        function show_access_buttons() {
-             frm.add_custom_button(
-                __('Create Folders'),function(){
-                    var folder=frm.doc.database_name
-                    var subfolders=frm.doc.tables
-                    var str = ''
-                    for (var i=0; i < subfolders.length; i ++) {
-                        str+="<br>"+folder+"/"+subfolders[i].table_name
-
-                    }
-                    frappe.confirm(__('Create Folders?'+str),()=>{
-                       
-                        frappe.call({method:'datamanagement.data_management.doctype.datasource.datasource.create_folders', args:{
-                                    'name':frm.doc.name
-                                },
-                                callback:function(r){
-                                    //frm.reload_doc();
-                                }
-                                })
-                            },()=>
-
-                            {
-                                // action to perform if No is selected
-                            }
-                    )
-                }
-                ,__('Actions')
-            );
-            };
 
             if (frm.doc.connection_type == 'DIRECT_DB'){
                 frm.set_df_property('ssh_tunnel_host','hidden',true)
--- datamanagement/data_management/doctype/datasink/datasink.py
+++ datamanagement/data_management/doctype/datasink/datasink.py
@@ -1,31 +1,24 @@
 # Copyright (c) 2023, Andy Garcia and contributors
 # For license information, please see license.txt
 
+# import frappe
+# Copyright (c) 2023, Andy Garcia and contributors
+# For license information, please see license.txt
+
 import frappe,boto3,json
 import datamanagement.data_management.doctype.aws_configuration.aws_configuration
 from frappe.model.document import Document
 
-class DataSource(Document):
+class DataSink(Document):
 	pass
 
 
 @frappe.whitelist()
-def fetch_details(datasource):
-	maindoc=frappe.get_doc('DataSource',datasource)
+def fetch_details(datasink):
+	maindoc=frappe.get_doc('DataSink',datasink)
 	if maindoc == None:
-		frappe.throw(f'Requested datasource {datasource} not found')
+		frappe.throw(f'Requested datasink {datasink} not found')
 	
-	tables = []
-	for item in maindoc.tables:
-		tableEntry={}
-		tableEntry['table_name']=item.table_name
-		partitions = json.loads(item.partitions)
-		tableEntry['paritions']=partitions
-		tableEntry['table_type']=item.table_type
-		tableEntry['last_processed_id']=item.last_processed_id
-		
-		tables.append(tableEntry)
-	print(f'TABLES={tables}')
 	result=[]
 	result.append({'method':maindoc.method})
 	result.append({'sqlType':maindoc.sql_type})
@@ -35,7 +28,7 @@ def fetch_details(datasource):
 	result.append({'dbName':maindoc.database_name})
 	result.append({'schemaName':maindoc.schema_name})
 
-	result.append({'tables':tables})
+	result.append({'table':maindoc.table})
 	result.append({'usr':maindoc.db_username})
 	result.append({'pwd':maindoc.db_password})
 	#result.append({'processDate':maindoc.process_date})
@@ -56,34 +49,10 @@ def fetch_details(datasource):
 
 
 
-@frappe.whitelist()
-def create_folders(name):
-	print('CREATE')
-	maindoc = frappe.get_doc('DataSource',name)
-	storage_type=maindoc.target_storage_type
-	resource=frappe.get_doc('Client Configuration').bucket_name
-	folder=maindoc.database_name
-
-	tables = []
-	for item in maindoc.tables:
-		tables.append(item.table_name)
-
-	print(f'storage={storage_type} folder={folder} tables={tables}')
-
-	if storage_type=='AWS S3':
-		datamanagement.data_management.doctype.aws_configuration.aws_configuration.import_aws_credentials()
-		# Let's use Amazon S3
-		s3 = boto3.client('s3')
-		s3.put_object(Bucket=resource,Key=folder)
-
-		for item in tables:
-			thisObject=folder+'/'+item
-			s3.put_object(Bucket=resource,Key=thisObject)
-
 
 @frappe.whitelist()
-def set_last_processed_id(datasource, table, value):
-	maindoc = frappe.get_doc('DataSource',datasource)
+def set_last_processed_id(datasink, table, value):
+	maindoc = frappe.get_doc('Datasink',datasink)
 
 	for item in maindoc.tables:
 		if item.table_name == table:
@@ -91,6 +60,6 @@ def set_last_processed_id(datasource, table, value):
 			maindoc.save()
 			return 'SUCCESS'
 		
-	frappe.throw(f'Table {table} does not exist in Data Source {datasource}')
+	frappe.throw(f'Table {table} does not exist in Data Source {datasink}')
 
 
