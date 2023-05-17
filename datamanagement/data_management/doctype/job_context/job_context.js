501a0ca4044a9bc6a2e0d8db25e6f35df2247dd7
Andy Garcia <andy@xurpas.com>
2023-05-17 15:28:28 +0800

    Added Job Context, Load Balancing

--- datamanagement/data_management/doctype/job_context/job_context.js
+++ datamanagement/data_management/doctype/job_context/job_context.js
@@ -6,12 +6,24 @@ frappe.ui.form.on("Job Context", {
         frappe.call({method:'datamanagement.data_management.doctype.job_context.job_context.fetch_client_data', args:{
         },
         callback:function(r){
-            console.log(r.message);
+            //console.log(r.message);
             frm.set_value("client_namespace",r.message.client_namespace);
             frm.set_value("storage_type",r.message.storage_type);
-            
+
+            frappe.call({method:'datamanagement.data_management.doctype.data_folder.data_folder.fetch_resource_names', args:{
+                storage_type:r.message.storage_type
+                },
+                callback:function(r){
+                    //console.log(r.message);
+                    frm.set_df_property('resource','options',r.message)
+                    
+                }
+                });
+                
         }
-        })
+        });
+       
+
 
 	},
 });
